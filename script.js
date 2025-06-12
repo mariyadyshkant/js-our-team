7ba9ebd72bd2b4ad2c03578229cc6346e32fa0c5
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-12 12:41:38 +0200

    Implement functions to create grid structure and cards for team display

--- script.js
+++ script.js
@@ -38,3 +38,34 @@ const teamMembers = [
 ];
 
 // Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+
+// Ho bisogno di una funzione per creare la struttura della griglia all'interno del cointainer di partenza
+
+function createRowElements(tag, ...classes) {
+  const divEl = document.createElement(tag);
+  divEl.classList.add(...classes);
+  document.querySelector(".container").appendChild(divEl);
+};
+
+createRowElements("div", "row", "d-flex", "flex-row", "flex-wrap", "justify-content-center");
+
+// Altra funzione per creare le colonne
+
+function createColElements(tag, ...classes) {
+  const divEl = document.createElement(tag);
+  divEl.classList.add(...classes);
+  document.querySelector(".row").appendChild(divEl);
+};
+
+createColElements("div", "col-12", "col-md-6", "col-lg-4", "d-flex", "justify-content-center");
+
+// Funzione per creare le card
+function createCardElements(tag, ...classes) {
+  const divEl = document.createElement(tag);
+  divEl.classList.add(...classes);
+  document.querySelector(".col-12").appendChild(divEl);
+};
+
+createCardElements("div", "card", "d-flex", "flex-row", "text-center");
+
+// Funzione per creare il contenuto della card
