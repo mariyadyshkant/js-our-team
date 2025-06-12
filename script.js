77fdd05cef6406f1130169a71c599a15ce652ce4
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-12 18:09:13 +0200

    Refactor team member data structure and update card creation functions

--- script.js
+++ script.js
@@ -1,71 +1,106 @@
 const teamMembers = [
   {
-    name: "Marco Bianchi",
+    member_name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "male1.png"
   },
   {
-    name: "Laura Rossi",
+    member_name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "female1.png"
   },
   {
-    name: "Giorgio Verdi",
+    member_name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "male2.png"
   },
   {
-    name: "Marta Ipsum",
+    member_name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "female2.png"
   },
   {
-    name: "Roberto Lorem",
+    member_name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "male3.png"
   },
   {
-    name: "Daniela Amet",
+    member_name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "female3.png"
   }
 ];
 
-// Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+// Creo la row una sola volta
+createRowElements("div", "row", "d-flex", "flex-row", "flex-wrap", "justify-content-center");
 
-// Ho bisogno di una funzione per creare la struttura della griglia all'interno del cointainer di partenza
+for (let i = 0; i < teamMembers.length; i++) {
+  const { member_name, role, email, img } = teamMembers[i];
 
+  // Creo la colonna per ogni membro
+  createColElements("div", "col-12", "col-md-6", "col-lg-4", "d-flex", "justify-content-center");
+  // Creo la card
+  createCardElements("div", "card", "d-flex", "flex-row", "text-center");
+  // Aggiungo l'immagine
+  createCardImgElements("img", img, "card-img-top", "rounded-circle", "w-50", "mt-3");
+  // Creo il corpo della card
+  createCardBodyElements("div", "card-body", "d-flex", "flex-column", "align-items-center");
+  // Titolo con il nome
+  createCardTitleElements("h5", member_name, "card-title", "mt-3");
+  // Testo con ruolo ed email
+  createCardTextElements("div", role, email, "card-text");
+}
+
+// Funzioni di creazione elementi
 function createRowElements(tag, ...classes) {
   const divEl = document.createElement(tag);
   divEl.classList.add(...classes);
   document.querySelector(".container").appendChild(divEl);
-};
-
-createRowElements("div", "row", "d-flex", "flex-row", "flex-wrap", "justify-content-center");
-
-// Altra funzione per creare le colonne
-
+  return divEl;
+}
 function createColElements(tag, ...classes) {
   const divEl = document.createElement(tag);
   divEl.classList.add(...classes);
   document.querySelector(".row").appendChild(divEl);
-};
-
-createColElements("div", "col-12", "col-md-6", "col-lg-4", "d-flex", "justify-content-center");
-
-// Funzione per creare le card
+  return divEl;
+}
 function createCardElements(tag, ...classes) {
   const divEl = document.createElement(tag);
   divEl.classList.add(...classes);
-  document.querySelector(".col-12").appendChild(divEl);
-};
-
-createCardElements("div", "card", "d-flex", "flex-row", "text-center");
-
-// Funzione per creare il contenuto della card
+  document.querySelector(".col-12:last-child").appendChild(divEl);
+  return divEl;
+}
+function createCardBodyElements(tag, ...classes) {
+  const divEl = document.createElement(tag);
+  divEl.classList.add(...classes);
+  document.querySelector(".card:last-child").appendChild(divEl);
+  return divEl;
+}
+function createCardTitleElements(tag, member_name, ...classes) {
+  const titleEl = document.createElement(tag);
+  titleEl.innerHTML = `<h5 class="card-title">${member_name}</h5>`;
+  document.querySelector(".card-body:last-child").appendChild(titleEl);
+}
+function createCardTextElements(tag, role, email, ...classes) {
+  const descriptionEl = document.createElement(tag);
+  descriptionEl.classList.add(...classes);
+  descriptionEl.innerHTML = `
+    <p class="card-text">${role}</p>
+    <p class="card-text">${email}</p>
+  `;
+  document.querySelector(".card-body:last-child").appendChild(descriptionEl);
+}
+function createCardImgElements(tag, img, ...classes) {
+  const imgEl = document.createElement(tag);
+  imgEl.classList.add(...classes);
+  imgEl.setAttribute("src", `./img/${img}`);
+  imgEl.setAttribute("alt", "team member image");
+  document.querySelector(".card:last-child").appendChild(imgEl);
+  return imgEl;
+}
