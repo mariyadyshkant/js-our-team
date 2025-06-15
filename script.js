b88dc34b6184c77711ae3df5b937061a4b769e11
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-15 20:31:21 +0200

    Refactor team member display and update image paths for consistency

--- script.js
+++ script.js
@@ -15,49 +15,52 @@ const teamMembers = [
     member_name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "male2.png"
+    img: "./img/male2.png"
   },
   {
     member_name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "female2.png"
+    img: "./img/female2.png"
   },
   {
     member_name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "male3.png"
+    img: "./img/male3.png"
   },
   {
     member_name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "female3.png"
+    img: "./img/female3.png"
   }
 ];
 
 // Creo la row una sola volta
-createRowElements("div", "row", "d-flex", "flex-row", "flex-wrap", "justify-content-center");
+const row = createRowElements("div", "row", "d-flex", "flex-row", "flex-wrap", "justify-content-center", "bg-light");
 
 for (let i = 0; i < teamMembers.length; i++) {
   const { member_name, role, email, img } = teamMembers[i];
 
   // Creo la colonna per ogni membro e salvo il riferimento
-  const col = createColElements("div", "col-4", "d-flex", "flex-row", "align-items-center", "my-3");
+  const col = createColElements("div", "col-4", "d-flex", "flex-row", "align-items-center", "justify-content-around", "my-3");
+  row.appendChild(col);
 
   // Creo il box immagine e inserisco l'immagine
   const imgBox = createImgBox("div", "me-3");
-  const imgEl = createImgElements("img", img, "w-75");
-  imgBox.appendChild(imgEl);
   col.appendChild(imgBox);
+  const imgEl = createImgElements("img", img, "w-100");
+  imgBox.appendChild(imgEl);
+
 
   // Creo il box descrizione e inserisco nome, ruolo, email
-  const descBox = createDescriptionBox("div", "d-flex", "flex-column");
-  descBox.appendChild(createTextElement("h5", member_name));
-  descBox.appendChild(createTextElement("p", role));
-  descBox.appendChild(createTextElement("p", email));
+  const descBox = createDescriptionBox("div", "d-flex", "flex-column", "justify-content-between", "lh-1");
   col.appendChild(descBox);
+  descBox.appendChild(createTextElement("h5", member_name, "fs-3", "mb-5", "bg-danger-subtle"));
+  descBox.appendChild(createTextElement("p", role, "fw-bolder"));
+  descBox.appendChild(createTextElement("p", email, "text-primary"));
+
 }
 
 // Funzioni di creazione elementi
@@ -70,28 +73,31 @@ function createRowElements(tag, ...classes) {
 function createColElements(tag, ...classes) {
   const divEl = document.createElement(tag);
   divEl.classList.add(...classes);
-  document.querySelector(".row").appendChild(divEl);
+  // document.querySelector(".row").appendChild(divEl);
   return divEl;
 }
 function createImgBox(tag, ...classes) {
-  const box = document.createElement(tag);
-  box.classList.add(...classes);
-  return box;
+  const imgBox = document.createElement(tag);
+  imgBox.classList.add(...classes);
+  // document.querySelector(".col").appendChild(imgBox);
+  return imgBox;
 }
 function createDescriptionBox(tag, ...classes) {
-  const box = document.createElement(tag);
-  box.classList.add(...classes);
-  return box;
+  const descBox = document.createElement(tag);
+  descBox.classList.add(...classes);
+  // document.querySelector(".col").appendChild(descBox);
+  return descBox;
 }
 function createImgElements(tag, img, ...classes) {
   const imgEl = document.createElement(tag);
   imgEl.classList.add(...classes);
-  imgEl.src = img.startsWith("./img/") ? img : `./img/${img}`;
+  imgEl.src = img;
   imgEl.alt = "team member image";
   return imgEl;
 }
-function createTextElement(tag, text) {
-  const el = document.createElement(tag);
-  el.textContent = text;
-  return el;
+function createTextElement(tag, text, ...classes) {
+  const textEl = document.createElement(tag);
+  textEl.classList.add(...classes);
+  textEl.textContent = text;
+  return textEl;
 }
